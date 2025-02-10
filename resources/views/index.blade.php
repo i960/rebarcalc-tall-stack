7e29f469799a1efa4bc618c17faff1f14c374b89
Jeff Kieke <2657820+i960@users.noreply.github.com>
2025-02-09 20:36:05 -0800

    Create base layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <x-slot name="header">
+        {{ __('RebarCalc') }}
+    </x-slot>
+    <div class="py-2">
+        <div class="max-w-7xl mx-auto">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <!-- content -->
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html class="h-full bg-gray-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name') }}</title>
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="h-full font-sans antialiased">
+        <div class="min-h-full">
+            @if (isset($header))
+                <header class="bg-white shadow-sm">
+                    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                        <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $header }}</h1>
+                    </div>
+                </header>
+            @endif
+
+            <main>
+                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    {{ $slot }}
+                </div>
+            </main>
+        </div>
+    </body>
+</html>

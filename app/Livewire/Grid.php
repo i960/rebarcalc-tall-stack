7e37507321009ebd686ae3c8194f5c4b8c64b69d
Jeff Kieke <2657820+i960@users.noreply.github.com>
2025-02-09 22:32:21 -0800

    Implement Grid component

--- app/Livewire/Grid.php
+++ app/Livewire/Grid.php
@@ -2,12 +2,74 @@
 
 namespace App\Livewire;
 
+use App\Services\Grid as RebarGrid;
+use App\Services\Svg;
 use Livewire\Component;
 
 class Grid extends Component
 {
+    public $widthFeet;
+    public $widthInches;
+    public $lengthFeet;
+    public $lengthInches;
+    public $spacing;
+
+    public $svg = '';
+
+    public function mount() {
+        $this->widthFeet = 10;
+        $this->widthInches = 0;
+        $this->lengthFeet = 10;
+        $this->lengthInches = 0;
+        $this->spacing = 24;
+
+        $this->calculate();
+    }
+
+    public function calculate() {
+        $this->validate();
+        $this->validateTotalWidth();
+        $this->validateTotalLength();
+
+        $width = $this->getTotalInches($this->widthFeet, $this->widthInches);
+        $length = $this->getTotalInches($this->lengthFeet, $this->lengthInches);
+
+        $grid = RebarGrid::createGrid(xLength: $width, yLength: $length, xSpacing: $this->spacing, ySpacing: $this->spacing, maxPieceLength: 240);
+
+        $this->svg = Svg::render($grid, $width, $length);
+    }
+
     public function render()
     {
         return view('livewire.grid');
+    }
+
+    protected function rules()
+    {
+        return [
+            'spacing' => 'required|numeric|min:6|max:48',
+        ];
+    }
+
+    protected function validateTotalWidth()
+    {
+        if ($this->getTotalInches($this->widthFeet, $this->widthInches) > 1200) {
+            $this->addError('totalWidth', 'The total width cannot exceed 100 feet.');
+        } else {
+            $this->resetErrorBag('totalWidth');
+        }
+    }
+
+    protected function validateTotalLength()
+    {
+        if ($this->getTotalInches($this->lengthFeet, $this->lengthInches) > 1200) {
+            $this->addError('totalLength', 'The total length cannot exceed 100 feet.');
+        } else {
+            $this->resetErrorBag('totalLength');
+        }
+    }
+
+    protected function getTotalInches(float $feet, float $inches): float {
+        return ($feet * 12) + $inches;
     }
 }
--- resources/views/livewire/grid.blade.php
+++ resources/views/livewire/grid.blade.php
@@ -1,3 +1,49 @@
-<div>
-    
+<div class="grid grid-cols-1 md:grid-cols-[1fr_280px] gap-4">
+    <div class="flex items-center justify-center bg-slate-50 rounded-md">
+        {!! $svg !!}
+    </div>
+    <form class="py-4 mr-6 rounded-md">
+        <div class="grid grid-cols-3 gap-2">
+            <label for="width" class="flex items-center justify-end">Width:</label>
+            <div class="flex space-x-2 col-span-2">
+                <div class="flex items-center border border-gray-700 bg-gray-700 rounded text-white focus-within:border-sky-500">
+                    <input wire:model="widthFeet" type="text" name="widthFeet" class="p-2 w-full rounded-l focus:outline-none">
+                    <span class="bg-gray-500 p-2 rounded-r">ft</span>
+                </div>
+                <div class="flex items-center border border-gray-700 bg-gray-700 rounded text-white focus-within:border-sky-500">
+                    <input wire:model="widthInches" type="text" name="widthInches" class="p-2 w-full rounded-l focus:outline-none">
+                    <span class="bg-gray-500 p-2 rounded-r">in</span>
+                </div>
+            </div>
+            <p class="text-red-500 text-sm mt-1 col-span-3">@error('totalWidth') {{ $message }} @enderror</p>
+
+            <label for="length" class="flex items-center justify-end">Length:</label>
+            <div class="flex space-x-2 col-span-2">
+                <div class="flex items-center border border-gray-700 bg-gray-700 rounded text-white focus-within:border-sky-500">
+                    <input wire:model="lengthFeet" type="text" name="lengthFeet" class="p-2 w-full rounded-l focus:outline-none">
+                    <span class="bg-gray-500 p-2 rounded-r">ft</span>
+                </div>
+                <div class="flex items-center border border-gray-700 bg-gray-700 rounded text-white focus-within:border-sky-500">
+                    <input wire:model="lengthInches" type="text" name="lengthInches" class="p-2 w-full rounded-l focus:outline-none">
+                    <span class="bg-gray-500 p-2 rounded-r">in</span>
+                </div>
+            </div>
+            <p class="text-red-500 text-sm mt-1 col-span-3">@error('totalLength') {{ $message }} @enderror</p>
+
+            <label for="spacing" class="flex items-center justify-end">Spacing:</label>
+            <div class="flex space-x-2 col-span-2">
+                <div class="flex items-center border border-gray-700 bg-gray-700 rounded text-white focus-within:border-sky-500">
+                    <input wire:model="spacing" type="text" name="spacing" class="p-2 w-full rounded-l focus:outline-none">
+                    <span class="bg-gray-500 p-2 rounded-r">in</span>
+                </div>
+            </div>
+            <p class="text-red-500 text-sm mt-1 col-span-3">@error('spacing') {{ $message }} @enderror</p>
+
+            <div class="col-span-3 text-center">
+                <button wire:click.prevent="calculate()" class="cursor-pointer text-white font-medium rounded-md p-2 bg-sky-600">
+                    Calculate
+                </button>
+            </div>
+        </div>
+    </form>
 </div>
